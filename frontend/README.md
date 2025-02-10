93c5c51bb19e58bad49c0424b905a95abfb92755
RupakBoral <boralrupak@gmail.com>
2025-02-10 19:43:12 +0530

    Components initiated

---
+++ frontend/README.md
@@ -0,0 +1,22 @@
+# Frontend
+
+- Project started with Vite-react
+- For css : Tailwind Css, DaisyUI
+- For routing : react-router-dom
+
+## Components
+
+- Body
+- Nav Bar
+- Login Page
+- SignUp Page
+- Profile
+- Feed
+- Footer
+
+## Routes
+
+- /login => login
+- /signUp => signUp
+- / => feed
+- /profile => profile
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,21 @@
+import Body from "./Components/Body";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+import SignUp from "./Components/SignUp";
+
 function App() {
   return (
     <div>
-      <div className="navbar bg-base-300">
-        <div className="flex-1 mx-5">
-          <a className="btn btn-ghost text-xl">🧑‍💻 Devity</a>
-        </div>
-        <div className="flex-none gap-2 mx-5">
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
