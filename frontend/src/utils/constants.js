645bde7a62887c95b50ad62455f60eaba9679783
RupakBoral <boralrupak@gmail.com>
2025-03-13 18:26:24 +0530

    update URL of backend

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,6 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:8888" : "/api";
+  location.hostname === "localhost"
+    ? "http://localhost:8888"
+    : "https://devity-backend.onrender.com";
+
+// while using AWS, write "/api" in place of "https://devity-backend.onrender.com"
